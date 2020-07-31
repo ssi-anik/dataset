27a2585a0e5d73e8c2d531d9b202bc87df57cdd6
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-08-01 03:40:55 +0600

    new functions added

--- src/CsvStorage.php
+++ src/CsvStorage.php
@@ -11,12 +11,40 @@ abstract class CsvStorage
     /** @var $reader Reader */
     private $reader = null;
 
+    /**
+     * If the document contains any header, position of the header
+     */
     protected function headerOffset () : ?int {
         return 0;
     }
 
+    /**
+     * If want to include the empty records in the result set
+     */
     protected function skipEmptyRecord () : bool {
         return true;
+    }
+
+    /**
+     * Array of stream filters
+     * https://csv.thephpleague.com/9.0/connections/filters/#adding-a-stream-filter
+     */
+    protected function streamFilters () : array {
+        return [];
+    }
+
+    /**
+     * No of rows to be fetched at a time
+     */
+    protected function limit () : int {
+        return 1000;
+    }
+
+    /**
+     * If want to execute the insert operations in a transaction
+     */
+    protected function useTransaction () : bool {
+        return false;
     }
 
     /**
@@ -44,6 +72,10 @@ abstract class CsvStorage
      */
     protected function exitOnError () : bool {
         return true;
+    }
+
+    private function extractColumnsForDb (array $required, array $record) : array {
+        return $record;
     }
 
     /**
@@ -108,6 +140,12 @@ abstract class CsvStorage
         $this->reader->setEscape($this->escapeCharacter());
         $this->reader->setHeaderOffset($this->headerOffset());
         $this->skipEmptyRecord() ? $this->reader->skipEmptyRecords() : $this->reader->includeEmptyRecords();
+
+        if ($this->reader->supportsStreamFilter() && ($filters = $this->streamFilters())) {
+            foreach ( $filters as $filter ) {
+                $this->reader->addStreamFilter($filter);
+            }
+        }
 
         return true;
     }
